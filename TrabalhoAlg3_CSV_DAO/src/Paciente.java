4be4619d7095ea16a9799da2e8c47901db189905
Antonio Gabriel <antoniogabrielti@gmail.com>
2016-04-19 15:02:08 -0300

    envio trabalho

---
+++ TrabalhoAlg3_CSV_DAO/src/Paciente.java
@@ -0,0 +1,20 @@
+
+public class Paciente {
+	private String rg;
+	private String nome;
+	private String dataNascimento;
+	public Paciente(String rg,String nome,String dataNascimento) {
+		this.nome=nome;
+		this.rg=rg;
+		this.dataNascimento=dataNascimento;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getRg() {
+		return rg;
+	}
+}
