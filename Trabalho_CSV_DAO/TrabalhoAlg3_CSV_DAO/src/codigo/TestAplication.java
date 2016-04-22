641d87fe8c4f66212708253a8be9b84c38be31ea
Antonio Gabriel <antoniogabrielti@gmail.com>
2016-04-21 22:51:36 -0300

    Atualizado 21/04 as 22:51

--- Trabalho_CSV_DAO/TrabalhoAlg3_CSV_DAO/src/codigo/TestAplication.java
+++ Trabalho_CSV_DAO/TrabalhoAlg3_CSV_DAO/src/codigo/TestAplication.java
@@ -10,19 +10,19 @@ public static void main(String[] args) {
     Paciente p4 = new Paciente("005","Nicole","14-08-2016");
     Paciente p5 = new Paciente("004","Joao","14-08-2016");
     Paciente p6 = new Paciente("003","Mariana","14-08-2016");
-    PacienteDaoVector ListaPaciente = new PacienteDaoVector();
-    ListaPaciente.addPaciente(p1);
-    ListaPaciente.addPaciente(p2);
-    ListaPaciente.addPaciente(p3);
-    ListaPaciente.addPaciente(p4);
-    ListaPaciente.addPaciente(p5);
-    ListaPaciente.addPaciente(p6);
+    PacienteDaoVector VetorPaciente = new PacienteDaoVector();
+    VetorPaciente.addPaciente(p1);
+    VetorPaciente.addPaciente(p2);
+    VetorPaciente.addPaciente(p3);
+    VetorPaciente.addPaciente(p4);
+    VetorPaciente.addPaciente(p5);
+    VetorPaciente.addPaciente(p6);
         System.out.println("XXXXXXXXXX ACHOU PELO RG XXXXXXXXXXXXX");
-    Paciente t = ListaPaciente.getPaciente("001");
+    Paciente t = VetorPaciente.getPaciente("001");
         System.out.println("Achado: "+t.toString());
         System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
         System.out.println("************ Vetor de Pacientes Ordenado pelo RG ************");
-    ListaPaciente.getVetor();
+    VetorPaciente.getVetor();
         System.out.println("************************************");
 
     }
